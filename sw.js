79e1f135f9a08af3f09606d839f666be3dba8d3e
fokokusadasi-tech <fokokusadasi@gmail.com>
2025-08-20 00:26:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='foko-cache-v4';
-const ASSETS=['./','./index.html','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME='foko-cache-v5';
+const ASSETS=['./','./index.html','./app.v5.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k))))); });
 self.addEventListener('fetch',e=>{
